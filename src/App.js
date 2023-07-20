cbca61d3e746cbc5f5d0528e8f7e8ed0d3a4ad7e
achujoseph0709 <achujozefsl0709@gmail.com>
2023-07-20 09:36:52 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
+import './App.css';
+import NavBar from './Components/NavBar/NavBar';
+import Banner from './Components/Banner/Banner';
+import RowPost from './Components/RowPost/RowPost'; 
+import {action,originals,comedy,horrorMovies,romanceMovies,documentaries} from './url'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+        <NavBar/>
+        <Banner/>
+        <RowPost  url={originals} title='NetFlix Orginals'/>
+        <RowPost  url={action} title='Action Movies' isSmall/>
+        <RowPost  url={comedy} title='Comedy Movies' isSmall/>
+        <RowPost  url={horrorMovies} title='Horror Movies' isSmall/>
+        <RowPost  url={romanceMovies} title='Romance Movies' isSmall/>
+        <RowPost  url={documentaries} title='Documentaries' isSmall/>
+        
     </div>
   );
 }
